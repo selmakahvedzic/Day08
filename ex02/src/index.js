0a8ab9edf4414f59e3ee79a95659990acbe12489
selmakahvedzic <selma.kahvedzic97@gmail.com>
2021-03-15 12:27:40 +0100

    Function with two parameters

---
+++ ex02/src/index.js
@@ -0,0 +1,9 @@
+//Write code below this line
+function functionPrintInfo(parameter1, parameter2) {
+  console.log(parameter1, parameter2);
+}
+
+functionPrintInfo("John", "23");
+functionPrintInfo("Michael", "43");
+//Write code abow this line
+module.exports = functionPrintInfo;
